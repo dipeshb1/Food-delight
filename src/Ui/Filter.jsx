2f850811270c199f3e63a451c263328612363421
Dipesh Bhagat <dipeshb164@gmail.com>
2024-08-08 11:10:39 +0530

    Minro change

--- src/Ui/Filter.jsx
+++ src/Ui/Filter.jsx
@@ -5,18 +5,18 @@ function Filter() {
   const currentSortValue = searchParams.get("filter") || "";
 
   const handleClick = () => {
+    const newParams = new URLSearchParams(searchParams.toString());
     if (currentSortValue) {
-      searchParams.delete("filter");
-      setSearchParams(searchParams);
+      newParams.delete("filter");
     } else {
-      searchParams.set("filter", "veg");
-      setSearchParams(searchParams);
+      newParams.set("filter", "veg");
     }
+    setSearchParams(newParams);
   };
 
   return (
     <button
-      className={`rounded-md border-2 border-green-500 bg-white px-2 py-1 text-sm text-green-900 transition-all duration-200 md:py-2 ${currentSortValue ? "bg-green-800 text-white ring-1 ring-green-900 ring-offset-2" : ""}`}
+      className={`rounded-md border-2 border-green-500 px-2 py-1 text-sm text-green-900 transition-all duration-200 md:py-2 ${currentSortValue ? "bg-green-800 text-white ring-1 ring-green-900 ring-offset-2" : "bg-white"}`}
       onClick={handleClick}
     >
       Pure Veg
